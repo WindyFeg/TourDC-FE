7afab143c328a93a6ebb9fd0ea580449b948d4f2
WindyFeg <phongspeed7@yahoo.com.vn>
2024-03-18 13:52:30 +0700

    Stable

---
+++ metro.config.js
@@ -0,0 +1,16 @@
+// const { getDefaultConfig } = require('@react-native-community/cli-platform-android');
+
+// const config = getDefaultConfig(__dirname);
+
+// config.resolver.extraNodeModules = {
+//     ...require("node-libs-expo"),
+// };
+
+// config.transformer.getTransformOptions = async () => ({
+//     transform: {
+//         experimentalImportSupport: false,
+//         inlineRequires: true,
+//     },
+// });
+
+// module.exports = config;
