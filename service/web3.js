da3c00e21541d05c88ac14afa5c196034f8a8be5
duync2006 <reynold.cs.w3@gmail.com>
2024-03-22 09:45:25 +0700

    add data on vibi chain #1

--- service/web3.js
+++ service/web3.js
@@ -1,29 +1,23 @@
-// DC Token Contract
-// 0x1A4091614adA249f08390E4836087A8cAC2f018A 
-
-// TOURISM Contract
-// 0xd3e7B4BF6FB03bdECbe5Db0b348285A22bF33Bf6
-
-// admin: 0x76E046c0811edDA17E57dB5D2C088DB0F30DcC74
-// import 'react-native-get-random-values';
-// import Web3 from "web3";
-// import DCToken_abi from "./DCToken_ABI.json"
-// import Tourism_abi from "./Tourism_ABI.json"
+import 'react-native-get-random-values';
+import Web3 from "web3";
+import DCToken_abi from "./DCToken_ABI.json"
+import Tourism_abi from "./Tourism_ABI.json"
+import DCToken_address from "../contracts/ERC20With4RMechanism-address.json"
+import Tourism_address from "../contracts/Tourism-address.json"
 
 // const { Web3 } = require('web3');
-import Web3 from "web3";
-const DCToken_abi = require("../contracts/ERC20With4RMechanism.json")
-const Tourism_abi = require("../contracts/Tourism.json")
-const DCToken_address = require("../contracts/ERC20With4RMechanism-address.json")
-const Tourism_address = require("../contracts/Tourism-address.json")
+// const DCToken_abi = require("../contracts/ERC20With4RMechanism.json")
+// const Tourism_abi = require("../contracts/Tourism.json")
+// const DCToken_address = require("../contracts/ERC20With4RMechanism-address.json")
+// const Tourism_address = require("../contracts/Tourism-address.json")
 
 
-const VBCProvider = "https://agd-seed-1.vbchain.vn/"
-const InfuraProvider = 'https://sepolia.infura.io/v3/c6b95d3b003e40cda8dcf76f7ba58be8'
+const VBCProvider = "https://vibi.vbchain.vn/"
+// const InfuraProvider = 'https://sepolia.infura.io/v3/c6b95d3b003e40cda8dcf76f7ba58be8'
 
-const localHost = "http://127.0.0.1:8545/"
+// const localHost = "http://127.0.0.1:8545/"
 // var web3Provider = Web3.providers.HttpProvider(InfuraProvider);
-var web3 = new Web3(localHost);
+var web3 = new Web3(VBCProvider);
 contract_DCToken = new web3.eth.Contract(DCToken_abi.abi, DCToken_address.Token)
 contract_4R = new web3.eth.Contract(Tourism_abi.abi, Tourism_address.Token)
 
@@ -152,15 +146,20 @@ const account3 = '0x9E0E58F9052aDc53986eA9ca7cf8389b0EdE364f'
 // account tourist 2: 0x2936E9fACfF3fb5DDc08d13DB19659ec093cdE69
 // account Tourist 3: 0x9E0E58F9052aDc53986eA9ca7cf8389b0EdE364f
 const test = async () => {
-  console.log(await getBalanceOf("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"))
-  console.log("getTouristInfor function: ", await getTouristInfor("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"))
-  console.log("get all reviews of tourist: ", await getTouristReviews("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"))
+  const owner = "0x76E046c0811edDA17E57dB5D2C088DB0F30DcC74";
+  const address1 = "0x1a620c351c07763f430897AeaA2883E37cA0aaCD"
+  const address2 = "0x9E0E58F9052aDc53986eA9ca7cf8389b0EdE364f"
+  const postID = "0x5a28a54a254168fde809b36b87260f51a07c299f232380cc09e5d6c271c2b77c"
+
+  console.log(await getBalanceOf(owner))
+  console.log("getTouristInfor function: ", await getTouristInfor(owner))
+  console.log("get all reviews of tourist: ", await getTouristReviews(owner))
   console.log("get all review of destination: ", await getDestinationReviews("65d6ec325ecf27cb3d803d87"))
-  console.log("get review By Post ID: ", await getReviewByPostID("0x8a8fa558cfc4192cb2010346a807b80881e0e81cf94547ec50d93cfce17a3c78"))
-  console.log("see postID list can get reward of account: ", await getRewardListsPostID("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"))
-  console.log("see reward of one postID of acc1: ", await getRewardPoinFromPostID("0x8a8fa558cfc4192cb2010346a807b80881e0e81cf94547ec50d93cfce17a3c78", "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"))
-  console.log("see reward of one postID of acc2: ", await getRewardPoinFromPostID("0x8a8fa558cfc4192cb2010346a807b80881e0e81cf94547ec50d93cfce17a3c78", "0x70997970C51812dc3A010C7d01b50e0d17dc79C8"))
-  console.log("see reward of one postID of acc3: ", await getRewardPoinFromPostID("0x8a8fa558cfc4192cb2010346a807b80881e0e81cf94547ec50d93cfce17a3c78", "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC"))
+  console.log("get review By Post ID: ", await getReviewByPostID(postID))
+  console.log("see postID list can get reward of account: ", await getRewardListsPostID(owner))
+  console.log("see reward of one postID of acc1: ", await getRewardPoinFromPostID(postID, owner))
+  console.log("see reward of one postID of acc2: ", await getRewardPoinFromPostID(postID, address1))
+  console.log("see reward of one postID of acc3: ", await getRewardPoinFromPostID(postID, address2))
 
 
   // console.log("get all rates of destination: ", await getDestinationRates("1"))
